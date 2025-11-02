252fa193c1eeab57e5c710b0b3a727620089551c
Mark Hallap <hallapmark@gmail.com>
2025-11-02 21:28:08 +0200

    Dynamically show button based on value of boolean

--- src/pages/LisaArvuti.jsx
+++ src/pages/LisaArvuti.jsx
@@ -2,17 +2,23 @@ import { useState } from "react";
 
 function LisaArvuti() {
   const [message, setMessage] = useState("Lisa arvuti!");
+  const [n2itaNuppu, uuendaN2itaNuppu] = useState(true);
+  
+  function addProduct() {
+    setMessage("Arvuti lisatud");
+    uuendaN2itaNuppu(false);
+  }
 
   return (
     <div>
-        <div>Sõnum: {message}</div>
-        <label>Mark</label> <br />
-        <input type="text" /> <br />
-        <label>Mudel</label> <br />
-        <input type="text" /> <br />
-        <label>Maksumus</label> <br />
-        <input type="number" /> <br />
-        {message === "Lisa arvuti!" && <button onClick={() => setMessage("Lisatud!")}>Sisesta</button>}
+      <div>Sõnum: {message}</div>
+      <label>Mark</label> <br />
+      <input type="text" /> <br />
+      <label>Mudel</label> <br />
+      <input type="text" /> <br />
+      <label>Maksumus</label> <br />
+      <input type="number" /> <br />
+      { n2itaNuppu === true && <button onClick={() => addProduct()}>Sisesta</button> }
     </div>
   )
 }
