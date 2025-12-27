8dac774ebcf6cd68ad1fa85593e8cbe4fdd92590
Mark Hallap <hallapmark@gmail.com>
2025-12-27 18:03:07 +0200

    Use localStorage to add computers, display on home

--- src/pages/Avaleht.jsx
+++ src/pages/Avaleht.jsx
@@ -1,8 +1,21 @@
 function Avaleht() {
+  const computers = JSON.parse(localStorage.getItem("laptops")) || [];
+
   return (
-    <div className ="avaleht-text">
+    <div className="avaleht-text">
       <div>Tere</div>
       <div>Siin lehel saad sülearvuteid vaadata ja lisada.</div>
+      <br />
+      { computers.map((oneComputer) => 
+        <div key={oneComputer.mark + oneComputer.mudel}>
+          <div>{oneComputer.mark}</div>
+          <div>{oneComputer.mudel}</div>
+          <div>{oneComputer.maksumus}</div>
+          <button>Teeme temaga midagi</button>
+          <br />
+          <br />
+        </div>
+      )}
     </div>
   )
 }
